605f9e9f7e24af614facd6c1d3fe24e6bb0e6b30
Tatyana Skripnik <skt@ciklum.com>
2019-10-10 17:58:49 +0300

    HW1: Final version

--- Exercises/data-types-overview/data-types-overview.js
+++ Exercises/data-types-overview/data-types-overview.js
@@ -3,10 +3,10 @@
 let stringVariable = 'inserted text';
 let numericVariable = 15;
 let booleanVariable = true;
-let objectVariable = {first: "objects first value"};
+let objectVariable = {first: 'objects first value'};
 let nullVariable = null;
 let undefinedVariable;
-let symbolVariable = Symbol("hidden");
+let symbolVariable = Symbol('hidden');
 let bigIntVariable = BigInt(Number.MAX_SAFE_INTEGER * 2);
 let stringConsturctorVariable = new String('string created using a constructor');
 let numberConsturctorVariable = new Number(123);
@@ -26,6 +26,19 @@ console.log(typeof(numberConsturctorVariable));
 console.log(typeof(booleanConsturctorVariable));
 
 //3. Demonstrate object's references.
+let parent = {name: 'Jack', age: 27, isAlive: true, children: {name: 'Amily', age: 2, isAlive: true}};
+let linkToParentObj = parent;
+let copyParentObj = Object.assign({}, parent);
+
+linkToParentObj.age = 30;
+copyParentObj.name = 'Dan';
+copyParentObj.children.age = 5;
+
+console.log('Original object:' + JSON.stringify(parent));
+console.log('Does linked object equal original? Answer: ' + (linkToParentObj == parent));
+console.log('Does copied object equal original? Answer: ' + (copyParentObj == parent) + ', BUT!');
+console.log('Are children objects inside copied and original object equal? Answer: '
+ + (copyParentObj.children == parent.children) + '. See copied object: ' + JSON.stringify(copyParentObj));
 
 
 //4. Demonstrate string's backticks features.
@@ -33,13 +46,13 @@ let backticksVariable = `This text will have a:
 - new line here
 - "${stringVariable}" from some string variable
 - value = ${numericVariable * 3} multiplied by 3 value from variable
-- "${objectVariable.first}" value from the first object key`
+- "${objectVariable.first}" value from the first object key`;
 
 console.log(backticksVariable);
 
 //# Additional
 //1. Declare an array containing different data types.
-let arrayVariable = ['text1', 1, 'text2', 2, 3, true, null]
+let arrayVariable = ['text1', 1, 'text2', 2, 3, true, null];
 
 //2. Iterate through it and collect amount of each data type to object.
 let objectTypesCounter = {};
@@ -47,10 +60,10 @@ let objectTypesCounter = {};
 arrayVariable.forEach(function(item) {
   if (objectTypesCounter[typeof item] === undefined) {
     objectTypesCounter[typeof item] = 1;
-    }
+  }
   else {
     objectTypesCounter[typeof item] += 1;
   }
-})
+});
 
 console.log(objectTypesCounter);
